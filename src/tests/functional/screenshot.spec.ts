6467487418e2bd33a55e3e29cb4708ebd16b7110
Ed Guzikowski <ed.guzikowski@ptml.com>
2022-11-11 15:27:04 +1300

    Trying new site

--- src/tests/functional/screenshot.spec.ts
+++ src/tests/functional/screenshot.spec.ts
@@ -12,10 +12,13 @@ test.describe('Tests for Visual Regression', () => {
    * [Test 2](https://github.com/Guzikowski/Playwright_E2E_API_Calls/wiki/My-Tests#test-2)
    */
   test('second screenshot', async ({ page }) => {
-    await page.goto('https://demo.playwright.dev/todomvc');
-    expect(await page.screenshot()).toMatchSnapshot('first-screenshot.png');
     await page.goto('https://playwright.dev/');
     expect(await page.screenshot()).toMatchSnapshot('second-screenshot.png');
+    expect(await page.screenshot()).not.toMatchSnapshot('first-screenshot.png');
+  });
+  test('third screenshot', async ({ page }) => {
+    await page.goto(process.env.SITE_URL as string);
+    expect(await page.screenshot()).toMatchSnapshot('third-screenshot.png');
     expect(await page.screenshot()).not.toMatchSnapshot('first-screenshot.png');
   });
 });
